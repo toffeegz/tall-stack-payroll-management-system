b64a149569727a68d46ac570eee8562dd8848292
gezrylclariz <gezrylclarizg@gmail.com>
2022-04-19 22:29:29 +0800

    new models and migrations, see description
    department, designation, loan, loan_installment

--- app/Models/Designation.php
+++ app/Models/Designation.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Designation extends Model
 {
     //
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'designation_user');
+    }
 }
