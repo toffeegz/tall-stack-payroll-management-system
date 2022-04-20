769eda9e3e7f705ec5fb2093fa02a02f2472bed2
gezrylclariz <gezrylclarizg@gmail.com>
2022-04-20 17:42:53 +0800

    model and migration holidays

---
+++ database/migrations/2022_03_19_051946_create_holidays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->boolean('is_legal')->comment('0=special,1=legal')->nullable();
+            $table->date('date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+};
