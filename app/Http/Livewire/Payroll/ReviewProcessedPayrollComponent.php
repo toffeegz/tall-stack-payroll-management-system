b2de23d1a9f762c3da96e7aa34ac79aebf59fb01
gezrylclariz <gezrylclarizg@gmail.com>
2022-04-21 15:55:42 +0800

    review processed payroll component (page 2 of run payroll)

---
+++ app/Http/Livewire/Payroll/ReviewProcessedPayrollComponent.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Livewire\Payroll;
+
+use Livewire\Component;
+use Illuminate\Http\Request;
+
+use App\Models\PayrollLog;
+
+use App\Classes\Payroll\PayrollClass;
+use App\Classes\Payroll\PayslipClass;
+
+use App\Models\PayrollPeriod;
+use App\Models\User;
+
+class ReviewProcessedPayrollComponent extends Component
+{
+    public $payroll_logs_id;
+    public $payroll_logs;
+    public $payrolls;
+
+    public $period_start;
+    public $period_end;
+    public $payout_date;
+
+    public $payroll_period_id;
+    
+    public $selected_user_id;
+    public $selected_user_name;
+    public $sss_amount = 0, $hdmf_amount = 0, $phic_amount= 0; 
+    public $total_tax_contributions;
+
+
+    public function mount(Request $request)
+    {
+        $this->payroll_logs_id = $request->id;
+        $this->payroll_logs = PayrollLog::find($this->payroll_logs_id);
+        if($this->payroll_logs)
+        {
+            $data = $this->payroll_logs;
+            $this->payrolls = PayrollClass::payroll($data);
+            // dd($this->payrolls);
+        }
+        else{
+            return abort(404);
+        }
+        
+        $payroll_period = PayrollPeriod::where('period_start', $this->payroll_logs->period_start)
+        ->where('period_end', $this->payroll_logs->period_end)
+        ->first();
+        $this->period_start = $payroll_period->period_start;
+        $this->period_end = $payroll_period->period_end;
+        $this->payout_date = $this->payroll_logs->payout_date;
+        $this->payroll_period_id = $payroll_period->id;
+
+
+        // 
+        $this->total_tax_contributions = 0;
+
+    }
+
+    public function render()
+    {
+        return view('livewire.payroll.review-processed-payroll-component',[
+            'total_net_pay' => $this->totalNetPay,
+        ])
+        ->layout('layouts.app',  ['menu' => 'payroll']);
+    }
+
+    public function getTotalNetPayProperty()
+    {
+        $net_pay = 0;
+        foreach($this->payrolls as $data)
+        {
+            $net_pay += $data['net_pay'];
+        }
+        return $net_pay;
+    }
+
+    public function editTaxContribution($user_id)
+    {
+        $this->selected_user_id = $user_id;
+        $user = User::find($user_id);
+        $this->selected_user_name = $user->formal_name();
+
+        $data = $this->payrolls[$user_id]['deductions_collection']['tax_contribution'];
+        // dd($data);
+        $this->sss_amount = $data['sss_contribution']['ee'];
+        $this->hdmf_amount = $data['hdmf_contribution']['total_ee'];
+        $this->phic_amount = $data['phic_contribution']['total_ee'];
+
+        $this->total_tax_contributions = $this->sss_amount + $this->hdmf_amount + $this->phic_amount;
+        $this->emit('openEditTaxContributionsModal');
+    }
+
+    public function submitTaxContributions()
+    {
+        $data = $this->payrolls[$this->selected_user_id]['deductions_collection']['tax_contribution'];
+
+        $total_tax_contributions = $this->sss_amount + $this->hdmf_amount + $this->phic_amount;
+
+        $this->payrolls[$this->selected_user_id]['tax_contributions'] = $total_tax_contributions;
+        $this->payrolls[$this->selected_user_id]['deductions_collection']['tax_contribution']['sss_contribution']['ee'] = $this->sss_amount;
+        $this->payrolls[$this->selected_user_id]['deductions_collection']['tax_contribution']['hdmf_contribution']['total_ee'] = $this->hdmf_amount;
+        $this->payrolls[$this->selected_user_id]['deductions_collection']['tax_contribution']['phic_contribution']['total_ee'] = $this->phic_amount;
+
+        $this->payrolls[$this->selected_user_id]['total_deductions'] = $this->payrolls[$this->selected_user_id]['loan_deductions'] + $this->payrolls[$this->selected_user_id]['tardiness_amount'] + $this->payrolls[$this->selected_user_id]['tax_contributions'];
+        $this->payrolls[$this->selected_user_id]['net_pay'] = $this->payrolls[$this->selected_user_id]['gross_pay'] - $this->payrolls[$this->selected_user_id]['total_deductions'];
+
+        $this->emit('closeEditTaxContributionsModal');
+
+    }
+
+    public function submitPayroll()
+    {
+        $data = PayslipClass::payslipCreation($this->payrolls);
+        $payroll_period = PayrollPeriod::find($this->payroll_period_id);
+        $payroll_period->is_payroll_generated = true;
+        $payroll_period->save();
+        return redirect()->route('payroll');
+    }
+}
---
+++ resources/views/livewire/payroll/review-processed-payroll-component.blade.php
@@ -0,0 +1,106 @@
+<div>
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="h-full overflow-y-auto px-6 md:px-32">
+        <div class="ml-12 md:ml-0 my-5 flex justify-between">
+            <div class="text-2xl font-semibold text-stone-700">
+                Review Payroll 
+            </div>
+            <div class="text-sm text-stone-500">
+                Pay period: 
+                {{ Carbon\Carbon::parse($period_start)->format('m/d/Y') . " - " . Carbon\Carbon::parse($period_end)->format('m/d/Y') }}
+            </div>
+        </div>
+
+
+        {{-- Table header --}}
+        <div class="my-10">
+            <div class="w-full flex pb-2 space-x-8">
+                <div class="flex flex-col">
+                    <span class="text-xs text-stone-500 font-semibold">Total Payroll</span>
+                    <span class="text-lg text-stone-700 font-bold">₱{{ number_format($total_net_pay, 2, '.', ',') }}</span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="text-xs text-stone-500 font-semibold">Period Start</span>
+                    <span class="text-md text-stone-700 font-semibold">{{ Carbon\Carbon::parse($period_start)->format('l M dS, Y') }}</span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="text-xs text-stone-500 font-semibold">Period Start</span>
+                    <span class="text-md text-stone-700 font-semibold">{{ Carbon\Carbon::parse($period_end)->format('l M dS, Y') }}</span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="text-xs text-stone-500 font-semibold">Payout Date</span>
+                    <span class="text-md text-stone-700 font-semibold">{{ Carbon\Carbon::parse($payout_date)->format('l M dS, Y') }}</span>
+                </div>
+            </div>
+            <div class="w-full  flex items-start">
+                <div class="flex justify-end space-x-4 ">
+                    <x-forms.button-rounded-md-primary wire:click="submitPayroll">
+                        Submit Payroll
+                    </x-forms.button-rounded-md-primary>
+                    {{-- <x-forms.button-rounded-md-secondary wire:click="saveForLater">
+                        Save for Later
+                    </x-forms.button-rounded-md-secondary> --}}
+                </div>
+            </div>
+        </div>
+
+        {{-- Employee Table --}}
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-x-auto">
+                <table class="w-full whitespace-no-wrap">
+                    <thead>
+                        <tr class="text-xs font-semibold tracking-wide text-left text-stone-500 uppercase border-b rounded-t-md bg-gray-50">
+                            <th class="px-2 md:px-4 py-3 text-right">Employee</th>
+                            <th class="px-2 md:px-4 py-3 text-right">Basic Pay</th>
+                            <th class="px-2 md:px-4 py-3 text-right">Gross Pay</th>
+                            <th class="px-2 md:px-4 py-3 text-right">Tax</th>
+                            <th class="px-2 md:px-4 py-3 text-right">Deductions</th>
+                            <th class="px-2 md:px-4 py-3 text-right">NetPay</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y">
+                        @foreach($payrolls as $payroll)
+                            <tr>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-stone-700 font-bold">{{ $payroll['full_name'] }}</p>
+                                </td>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-right text-stone-600 font-bold">
+                                        ₱{{ number_format($payroll['basic_pay'], 2, '.', ',') }}
+                                    </p>
+                                </td>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-right text-stone-600 font-bold">
+                                        ₱{{ number_format($payroll['gross_pay'], 2, '.', ',') }}
+                                    </p>
+                                </td>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-right text-stone-600 font-bold" wire:key="{{ $payroll['user_id'] }}.{{ $payroll['tax_contributions'] }}">
+                                        ₱{{ number_format($payroll['tax_contributions'], 2, '.', ',') }}
+                                        @if($payroll['is_tax_exempted'] == false)
+                                            <a class="cursor-pointer text-blue-500 ml-2" wire:click="editTaxContribution({{ $payroll['user_id'] }})"><i class="fa-solid fa-pen"></i></a>
+                                        @endif
+                                    </p>
+                                </td>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-right text-stone-600 font-bold">
+                                        ₱{{ number_format($payroll['total_deductions'], 2, '.', ',') }}
+                                    </p>
+                                </td>
+                                <td class="px-2 md:px-4 py-3 align-top">
+                                    <p class=" text-xs text-right text-stone-600 font-bold">
+                                        ₱{{ number_format($payroll['net_pay'], 2, '.', ',') }}
+                                    </p>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            {{--  --}}
+            
+        </div>
+    </div>
+    @include('scripts.payroll.review-payroll-script')
+    @include('modals.payroll.review-payroll-modal')
+</div>
