f017cf7aaf9e5fde1923d20a4ac7a44cb45028ea
gezrylclariz <gezrylclarizg@gmail.com>
2022-04-25 01:08:52 +0800

    settings: tax contributions -> sss model

--- app/Models/SssContributionModel.php
+++ app/Models/SssContributionModel.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SssContributionRate;
 
 class SssContributionModel extends Model
 {
     use HasFactory;
+
+    public function sss_rate()
+    {
+        return $this->belongsTo(SssContributionRate::class);
+    }
 }
--- app/Models/SssContributionRate.php
+++ app/Models/SssContributionRate.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SssContributionModel;
 
 class SssContributionRate extends Model
 {
     use HasFactory;
+    public function sss_contributions()
+    {
+        return $this->hasMany(SssContributionModel::class);
+    }
 }
