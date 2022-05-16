0da034077d35f7808b7f7f877c49424465febf53
gezrylclariz <gezrylclarizg@gmail.com>
2022-05-17 02:05:35 +0800

    route api: generate code

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['namespace' => 'App\Http\Controllers'], function() {
+    Route::get('generate-code/{key}', function($key) {
+        $year = Carbon::now()->format('Y');
+        $data = $year . "-" . sprintf('%04d', $key);
+        return $data;
+    });
+});
+
