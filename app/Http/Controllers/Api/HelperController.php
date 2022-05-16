849580184e35fdbd9306ec2f9b021ac39db35c14
gezrylclariz <gezrylclarizg@gmail.com>
2022-05-17 02:05:17 +0800

    Create HelperController.php

---
+++ app/Http/Controllers/Api/HelperController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Helper;
+
+class HelperController extends Controller
+{
+    
+}
