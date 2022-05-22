2ea21d36c492762c8b84dda827e6a0e9ee26357b
gezrylclariz <gezrylclarizg@gmail.com>
2022-05-22 16:34:25 +0800

    draft logs table

---
+++ database/migrations/2022_05_19_200617_create_draft_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDraftLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('draft_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('code')->unique();
+            $table->string('name');
+            $table->json('value');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('draft_logs');
+    }
+}
