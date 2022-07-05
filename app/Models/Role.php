35ac5938eace60807578dc138358aa83f9097456
gezrylclariz <gezrylclarizg@gmail.com>
2022-07-05 20:50:45 +0800

    Update Role.php
    include hasfactory

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Laratrust\Models\LaratrustRole;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends LaratrustRole
 {
+    use HasFactory;
+    
     public $guarded = [];
 }
