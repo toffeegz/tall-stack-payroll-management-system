c2cd8e752f75d26372707f7740da45419aa939b9
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-02 11:00:53 +0800

    Profile component -> added personal information

--- app/Http/Livewire/Employee/ProfileComponent.php
+++ app/Http/Livewire/Employee/ProfileComponent.php
@@ -19,15 +19,18 @@ class ProfileComponent extends Component
     public $last_name = "";
     public $first_name = "";
     public $middle_name = "";
+    public $suffix_name = "";
     public $code = "";
     public $email = "";
     public $phone_number = "";
     public $gender = "";
     public $marital_status = "";
     public $birth_date = "";
+    public $birth_place = "";
     public $nationality = "";
     public $fathers_name = "";
     public $mothers_name = "";
+    public $number_dependent = "";
     public $address = "";
 
     // 
@@ -62,45 +65,8 @@ class ProfileComponent extends Component
         {
             return abort(404);
         } else {
-            $this->last_name = $this->user->last_name;
-            $this->first_name = $this->user->first_name;
-            $this->middle_name = $this->user->middle_name;
-            $this->code = $this->user->code;
-            $this->email = $this->user->email;
-            $this->phone_number = $this->user->phone_number;
-            $this->gender = $this->user->gender;
-            $this->marital_status = $this->user->marital_status;
-            $this->birth_date = $this->user->birth_date;
-            $this->nationality = $this->user->nationality;
-            $this->fathers_name = $this->user->fathers_name;
-            $this->mothers_name = $this->user->mothers_name;
-            $this->address = $this->user->address;
-
-            $this->employment_status = $this->user->employment_status;
-            $this->hired_date = $this->user->hired_date;
-            $this->is_active = $this->user->is_active;
-
-            if($this->user->deleted_at) 
-            {
-                $this->is_archive = true;
-            }
-
-            if($this->user->latestDesignation())
-            {
-                $this->designation_id = $this->user->latestDesignation()->id;
-                $this->department_id = $this->user->latestDesignation()->department->id;
-            }
-
-            $this->is_tax_exempted = $this->user->is_tax_exempted;
-            $this->is_paid_holidays = $this->user->is_paid_holidays;
-
-            $this->sss_number = $this->user->sss_number;
-            $this->hdmf_number = $this->user->hdmf_number;
-            $this->phic_number = $this->user->phic_number;
-
-
+            $this->loadUser();
         }
-
 
     }
 
@@ -142,87 +108,6 @@ class ProfileComponent extends Component
         $this->daily_rate = Designation::find($value)->value('daily_rate');
     }  
 
-
-    // submit
-
-    public function submitPersonalInformation()
-    {
-        $this->validate([
-            'last_name' => 'required|string|min:2|max:255',
-            'first_name' => 'required|string|min:2|max:255',
-            'middle_name' => 'nullable|string|min:2|max:255',
-            // 'code' => 'required|unique:users,code',
-            'email' => 'required|email|unique:users',
-            'phone_number' => 'required|numeric',
-            'gender' => 'required|numeric',
-            'marital_status' => 'required|numeric',
-            'birth_date' => 'required|date|before:today',
-            'nationality' => 'required|',
-            'fathers_name' => 'nullable',
-            'mothers_name' => 'nullable',
-            'address' => 'nullable|max:255',
-        ]);
-
-        $this->user->last_name = $this->last_name;
-        $this->user->first_name = $this->first_name;
-        $this->user->middle_name = $this->middle_name;
-        $this->user->code = $this->code;
-        $this->user->email = $this->email;
-        $this->user->phone_number = $this->phone_number;
-        $this->user->gender = $this->gender;
-        $this->user->marital_status = $this->marital_status;
-        $this->user->birth_date = $this->birth_date;
-        $this->user->nationality = $this->nationality;
-        $this->user->fathers_name = $this->fathers_name;
-        $this->user->mothers_name = $this->mothers_name;
-        $this->user->address = $this->address;
-
-        $this->user->save();
-
-        $this->emit('closePersonalInformationModal');
-    }
-
-    public function submitEmploymentDetails()
-    {
-        $this->validate([
-            'employment_status' => 'required|numeric',
-            'hired_date' => 'required|date|',
-            'is_active' => 'required|boolean',
-        ]);
-
-        $this->user->employment_status = $this->employment_status;
-        $this->user->hired_date = $this->hired_date;
-        $this->user->is_active = $this->is_active;
-
-        $this->user->save();
-        $this->emit('closeEmploymentDetailsModal');
-
-    }
-    
-    public function submitCompensation()
-    {
-        $this->validate([
-            'designation_id' => 'required|numeric',
-            'sss_number' => 'nullable|max:255',
-            'hdmf_number' => 'nullable|max:255',
-            'phic_number' => 'nullable|max:255',
-        ]);
-
-        $this->user->sss_number = $this->sss_number;
-        $this->user->hdmf_number = $this->hdmf_number;
-        $this->user->phic_number = $this->phic_number;
-        $this->user->save();
-        
-        $new_designation = DB::table('designation_user')->insert([
-            'user_id' => $this->user->id,
-            'designation_id' => $this->designation_id,
-            "created_at" => Carbon::now(), # new \Datetime()
-            "updated_at" => Carbon::now(),  # new \Datetime()
-        ]);
-        
-        $this->emit('closeCompensationModal');
-    }
-
     public function updatedisArchive($val)
     {
         if($val == true) {
@@ -232,5 +117,74 @@ class ProfileComponent extends Component
             $this->user->deleted_at = null;
             $this->user->save();
         }
+    }
+
+    // update
+    public function updatePersonalInformation()
+    {
+        $this->user->first_name = $this->first_name;
+        $this->user->middle_name = $this->middle_name;
+        $this->user->last_name = $this->last_name;
+        $this->user->suffix_name = $this->suffix_name;
+        $this->user->phone_number = $this->phone_number;
+        $this->user->email = $this->email;
+        $this->user->gender = $this->gender;
+        $this->user->marital_status = $this->marital_status;
+        $this->user->nationality = $this->nationality;
+        $this->user->birth_date = $this->birth_date;
+        $this->user->birth_place = $this->birth_place;
+        $this->user->fathers_name = $this->fathers_name;
+        $this->user->mothers_name = $this->mothers_name;
+        $this->user->number_dependent = $this->number_dependent;
+        $this->user->address = $this->address;
+        $this->user->save();
+
+        $this->loadUser();
+
+        $this->emit('openNotifModal');
+    }
+
+    // load
+    public function loadUser()
+    {
+        $this->last_name = $this->user->last_name;
+        $this->first_name = $this->user->first_name;
+        $this->middle_name = $this->user->middle_name;
+        $this->suffix_name = $this->user->suffix_name;
+        $this->code = $this->user->code;
+        $this->email = $this->user->email;
+        $this->phone_number = $this->user->phone_number;
+        $this->gender = $this->user->gender;
+        $this->marital_status = $this->user->marital_status;
+        $this->birth_date = $this->user->birth_date;
+        $this->birth_place = $this->user->birth_place;
+        $this->nationality = $this->user->nationality;
+        $this->fathers_name = $this->user->fathers_name;
+        $this->mothers_name = $this->user->mothers_name;
+        $this->number_dependent = $this->user->number_dependent;
+        $this->address = $this->user->address;
+
+        $this->employment_status = $this->user->employment_status;
+        $this->hired_date = $this->user->hired_date;
+        $this->is_active = $this->user->is_active;
+
+        if($this->user->deleted_at) 
+        {
+            $this->is_archive = true;
+        }
+
+        if($this->user->latestDesignation())
+        {
+            $this->designation_id = $this->user->latestDesignation()->id;
+            $this->department_id = $this->user->latestDesignation()->department->id;
+        }
+
+        $this->is_tax_exempted = $this->user->is_tax_exempted;
+        $this->is_paid_holidays = $this->user->is_paid_holidays;
+
+        $this->sss_number = $this->user->sss_number;
+        $this->hdmf_number = $this->user->hdmf_number;
+        $this->phic_number = $this->user->phic_number;
+
     }
 }
