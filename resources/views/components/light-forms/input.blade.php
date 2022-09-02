0568297989c244c2b2d099dccf39028a14fdaac7
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-02 12:18:56 +0800

    light forms input

--- resources/views/components/light-forms/input.blade.php
+++ resources/views/components/light-forms/input.blade.php
@@ -1,2 +1,2 @@
-@props(['disabled' => false])
-<input {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'w-full text-sm font-semibold rounded-md border-stone-200 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 disabled:bg-stone-50']) }}>
+
+<input {{ isset($disabled) ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'w-full text-sm font-semibold rounded-md border-stone-200 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 disabled:bg-stone-50']) }}>
