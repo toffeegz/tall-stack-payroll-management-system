a810450f009297ceac4cf83cb0cf0c41bf4dbd5f
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-02 12:19:19 +0800

    profile component -> employment section front end

--- resources/views/livewire/employee/profile-component.blade.php
+++ resources/views/livewire/employee/profile-component.blade.php
@@ -1,6 +1,26 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
     <div class="h-full overflow-y-auto px-6 ">
+        
+        @if($errors->any())
+            <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-stone-900 bg-red-50 rounded-xl border border-red-100 focus:outline-none focus:shadow-outline-stone">
+                <div class="flex flex-row space-x-4 w-full">
+                    <div class="flex items-center bg-red-100 rounded-md p-2 h-16">
+                        <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48" style=" fill:#f63b3b;"><path d="M 24 4 C 12.972292 4 4 12.972292 4 24 C 4 27.275316 4.8627078 30.334853 6.2617188 33.064453 L 4.09375 40.828125 C 3.5887973 42.631528 5.3719261 44.41261 7.1757812 43.908203 L 14.943359 41.740234 C 17.671046 43.137358 20.726959 44 24 44 C 35.027708 44 44 35.027708 44 24 C 44 12.972292 35.027708 4 24 4 z M 24 7 C 33.406292 7 41 14.593708 41 24 C 41 33.406292 33.406292 41 24 41 C 20.997029 41 18.192258 40.218281 15.744141 38.853516 A 1.50015 1.50015 0 0 0 14.609375 38.71875 L 7.2226562 40.78125 L 9.2851562 33.398438 A 1.50015 1.50015 0 0 0 9.1503906 32.263672 C 7.7836522 29.813476 7 27.004518 7 24 C 7 14.593708 14.593708 7 24 7 z M 23.976562 12.978516 A 1.50015 1.50015 0 0 0 22.5 14.5 L 22.5 26.5 A 1.50015 1.50015 0 1 0 25.5 26.5 L 25.5 14.5 A 1.50015 1.50015 0 0 0 23.976562 12.978516 z M 24 31 A 2 2 0 0 0 24 35 A 2 2 0 0 0 24 31 z"></path></svg>
+                    </div>
+                    <div class="w-full">
+                        <div class="font-bold text-base text-red-600">Oh no! There was an error</div>
+                        <div class="text-stone-500 text-xs font-light grid grid-cols-3 w-full">
+                            @foreach ($errors->all() as $error)
+                                <div>{{ $error }}</div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+
         <div class="grid grid-cols-1 md:grid-cols-12 gap-6 mt-6">
             
             {{-- left panel --}}
@@ -101,26 +121,6 @@
                         {{-- @livewire('employee.profile.personal-information-component', ['user_id'=> $user->id]) --}}
                         {{-- Personal Information component --}}
                         <div class="space-y-4">
-
-                            @if($errors->any())
-                                <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-stone-900 bg-red-50 rounded-xl border border-red-100 focus:outline-none focus:shadow-outline-stone">
-                                    <div class="flex flex-row space-x-4 w-full">
-                                        <div class="flex items-center bg-red-100 rounded-md p-2 h-16">
-                                            <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48" style=" fill:#f63b3b;"><path d="M 24 4 C 12.972292 4 4 12.972292 4 24 C 4 27.275316 4.8627078 30.334853 6.2617188 33.064453 L 4.09375 40.828125 C 3.5887973 42.631528 5.3719261 44.41261 7.1757812 43.908203 L 14.943359 41.740234 C 17.671046 43.137358 20.726959 44 24 44 C 35.027708 44 44 35.027708 44 24 C 44 12.972292 35.027708 4 24 4 z M 24 7 C 33.406292 7 41 14.593708 41 24 C 41 33.406292 33.406292 41 24 41 C 20.997029 41 18.192258 40.218281 15.744141 38.853516 A 1.50015 1.50015 0 0 0 14.609375 38.71875 L 7.2226562 40.78125 L 9.2851562 33.398438 A 1.50015 1.50015 0 0 0 9.1503906 32.263672 C 7.7836522 29.813476 7 27.004518 7 24 C 7 14.593708 14.593708 7 24 7 z M 23.976562 12.978516 A 1.50015 1.50015 0 0 0 22.5 14.5 L 22.5 26.5 A 1.50015 1.50015 0 1 0 25.5 26.5 L 25.5 14.5 A 1.50015 1.50015 0 0 0 23.976562 12.978516 z M 24 31 A 2 2 0 0 0 24 35 A 2 2 0 0 0 24 31 z"></path></svg>
-                                        </div>
-                                        <div class="w-full">
-                                            <div class="font-bold text-base text-red-600">Oh no! There was an error</div>
-                                            <div class="text-stone-500 text-xs font-light grid grid-cols-3 w-full">
-                                                @foreach ($errors->all() as $error)
-                                                    <div>{{ $error }}</div>
-                                                @endforeach
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif
-                    
-                    
                             {{-- name --}}
                             <div class="grid grid-cols-7 gap-4">
                     
@@ -279,7 +279,118 @@
                             </div>
                         </div>
                     @elseif($page_name == "employment")
-                        @livewire('employee.profile.employment-component', ['user_id'=> $user->id])
+                        {{-- @livewire('employee.profile.employment-component', ['user_id'=> $user->id]) --}}
+                        <div class="space-y-4">
+                            {{-- employment_details --}}
+                            <div class="grid grid-cols-2 gap-4">
+                    
+                                {{-- employment_status --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        Employment Status<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.select wire:model="employment_status">
+                                        <option value="">- select draft -</option>
+                                        @foreach(config('company.employment_status') as $key => $employment_status)
+                                            <option value="{{ $key }}">{{ $employment_status }}</option>
+                                        @endforeach 
+                                    </x-light-forms.select>
+                                </div>
+                    
+                                {{-- hired_date --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        Hired Date
+                                    </x-light-forms.label>
+                                    <x-light-forms.input type="date" wire:model.defer="hired_date"></x-light-forms.input>
+                                </div>
+                            </div>
+
+                            <div class="grid grid-cols-3 gap-4">
+                    
+                                {{-- department_id --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        Department<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.select wire:model="department_id">
+                                        <option value="">- select department -</option>
+                                        @foreach($departments as $department)
+                                            <option value="{{ $department->id }}">{{ $department->name }}</option>
+                                        @endforeach 
+                                    </x-light-forms.select>
+                                </div>
+                    
+                                {{-- designation_id --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        Designation<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.select wire:model="designation_id">
+                                        <option value="">- select designation -</option>
+                                        @foreach($designations as $designation)
+                                            <option value="{{ $designation->id }}">{{ $designation->name }}</option>
+                                        @endforeach 
+                                    </x-light-forms.select>
+                                </div>
+
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        Daily Rate<span class="text-red-400 ml-1"></span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.input type="text" :disabled="true" wire:model="daily_rate">
+                                    </x-light-forms.input>
+                                </div>
+                            </div>
+                    
+                            <div class="w-full flex mb-2 pt-8">
+                                <div class="flex-none text-stone-500 text-sm font-bold tracking-wide">
+                                    Tax Details
+                                </div>
+                                <hr class="text-stone-500 flex-auto my-2 mx-2">
+                            </div>
+                            {{-- tax_details --}}
+                            <div class="grid grid-cols-3 gap-4">
+                    
+                                {{-- employment_status --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        SSS Number<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.input type="text" wire:model="sss_number"></x-light-forms.input>
+                                </div>
+                    
+                                {{-- hdmf_number --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        HDMF Number<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.input type="text" wire:model="hdmf_number"></x-light-forms.input>
+                                </div>
+
+                                {{-- phic_number --}}
+                                <div class="col-span-1 space-y-1">
+                                    <x-light-forms.label>
+                                        PHIC Number<span class="text-red-400 ml-1">*</span>
+                                    </x-light-forms.label>
+                                    <x-light-forms.input type="text" wire:model="phic_number"></x-light-forms.input>
+                                </div>
+                            </div>
+
+                            <div class="px-4 pt-5">
+                                <x-forms.checkbox wire:model="is_tax_exempted"></x-forms.checkbox>
+                                <x-forms.checkbox-label>
+                                    Tax Exempted
+                                </x-forms.checkbox-label>
+                            </div>
+                            <div class="px-4">
+                                <x-forms.checkbox wire:model="is_paid_holidays"></x-forms.checkbox>
+                                <x-forms.checkbox-label>
+                                    Paid Holidays
+                                </x-forms.checkbox-label>
+                            </div>
+                        </div>
+                        
                     @endif
                 </div>
             </div>
