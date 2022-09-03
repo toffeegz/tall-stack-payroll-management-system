37579583e4cd658f53ef58338df11b738e00cf9f
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-03 18:50:35 +0800

    User seeder with designation full stack dev (test purposes)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 
 use App\Models\User;
 use App\Models\Role;
+use App\Models\Designation;
 use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
@@ -42,7 +43,31 @@ class UserSeeder extends Seeder
             'frequency_id' => 1,
         ];
 
+        // $user = [
+        //     'email' => 'gabriellegallego@gmail.com',
+        //     'password' => Hash::make('password'),
+        //     'last_name' => 'Testing',
+        //     'first_name' => 'Test',
+        //     'middle_name' => 'Test',
+        //     'code' => '2022-0003',
+        //     'phone_number' => '0975935907',
+        //     'birth_date' => '1999-05-27',
+        //     'birth_place' => 'Binan, Laguna',
+        //     'fathers_name' => 'Fernando Gallego',
+        //     'mothers_name' => 'Rosalinda Beato',
+        //     'gender' => 1,
+        //     'marital_status' => 1,
+        //     'nationality' => 'Filipino',
+        //     'address' => 'Gma, Cavite',
+        //     'employment_status' => 1,
+        //     'is_active' => true,
+        //     'is_paid_holidays' => true,
+        //     'is_tax_exempted' => false,
+        //     'frequency_id' => 1,
+        // ];
+
         $user = User::create($user);
         $user->attachRole(Role::ADMINISTRATOR_ID);
+        $user->designations()->attach([Designation::FULL_STACK_DEVELOPER_ID]);
     }
 }
