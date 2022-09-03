17a99a4e7dcf242ef83b025a7473c1a28742992f
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-03 18:47:27 +0800

    Create EmploymentRequest.php

---
+++ app/Http/Requests/User/EmploymentRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmploymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'employment_status' => ['required', 'numeric'],
+            'hired_date' => ['required', 'date'],
+            'designation_id' => ['required', 'numeric'],
+            'sss_number' => ['required'],
+            'hdmf_number' => ['required'],
+            'phic_number' => ['required'],
+            'is_tax_exempted' => ['required', 'boolean'],
+            'is_paid_holidays' => ['required', 'boolean'],
+        ];
+    }
+}
