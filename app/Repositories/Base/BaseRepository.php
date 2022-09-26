b4031e6f231260d65acefe6ef1a683806dad1075
gezrylclariz <gezrylclarizg@gmail.com>
2022-09-27 07:51:53 +0800

    initial BaseRepository

--- app/Repositories/Base/BaseRepository.php
+++ app/Repositories/Base/BaseRepository.php
@@ -22,4 +22,67 @@ abstract class BaseRepository implements BaseRepositoryInterface
 
         return $this->model->filter($search)->orderBy($sortByColumn, $sortBy)->paginate(request('limit') ?? 10);
     }
+
+    public function show(string $id, $with = [])
+    {
+        return $this->model->with($with)->withTrashed()->findOrFail($id);
+    }
+
+    public function update(array $params, string $id)
+    {
+        DB::beginTransaction();
+        try {
+            $result = $this->model->findOrFail($id);
+            $result->update($params);
+            DB::commit();
+            return $this->model->findOrFail($id);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $e->getMessage();
+        }
+    }
+    
+    public function create(array $params)
+    {
+        DB::beginTransaction();
+        try {
+            $data = $this->model->create($params);
+            DB::commit();
+            return $data;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $e->getMessage();
+        }
+    }
+
+    public function secureDelete(string $id, array $relations)
+    {
+        DB::beginTransaction();
+        try {
+            $result = $this->model->findOrFail($id);
+            $hasRelation = $result->secureDelete($relations);
+            if($hasRelation == false) {
+                $result->delete();
+            }
+            DB::commit();
+            return $hasRelation == true ? false : $result;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $e->getMessage();
+        }
+    }
+
+    public function restore(string $id)
+    {
+        DB::beginTransaction();
+        try {
+            $result = $this->model->onlyTrashed()->findOrFail($id);
+            $result->restore();
+            DB::commit();
+            return $result;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $e->getMessage();
+        }
+    }
 }
--- app/Repositories/Base/BaseRepositoryInterface.php
+++ app/Repositories/Base/BaseRepositoryInterface.php
@@ -5,4 +5,8 @@ namespace App\Repositories\Base;
 interface BaseRepository 
 {
     public function items(array $search, array $relations);
+    public function show(string $id, $with = []);
+    public function update(array $params, string $id);
+    public function create(array $params);
+    public function secureDelete(string $id, array $relations);
 }
