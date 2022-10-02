ab5c7d0b2788c40918ff9c4ef8de4f2c93500aea
gezrylclariz <gezrylclarizg@gmail.com>
2022-10-02 19:04:08 +0800

    PayrollLog Repository

---
+++ app/Repositories/PayrollLog/PayrollLogRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\PayrollLog;
+
+use App\Repositories\Base\BaseRepositoryInterface;
+
+interface PayrollLogRepositoryInterface extends BaseRepositoryInterface
+{
+    //
+}
