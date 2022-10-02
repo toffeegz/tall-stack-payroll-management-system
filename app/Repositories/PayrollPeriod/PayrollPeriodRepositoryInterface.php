fd8da16dc6c85127603d8556b72f3bd88f3d8c7f
gezrylclariz <gezrylclarizg@gmail.com>
2022-10-02 19:04:46 +0800

    PayrollPeriod Repository

---
+++ app/Repositories/PayrollPeriod/PayrollPeriodRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\PayrollPeriod;
+
+use App\Repositories\Base\BaseRepositoryInterface;
+
+interface PayrollPeriodRepositoryInterface extends BaseRepositoryInterface
+{
+    //
+}
