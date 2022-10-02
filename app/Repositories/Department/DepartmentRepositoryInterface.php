ba7e84309cea7d59b999ffe5b66c017976bda3c6
gezrylclariz <gezrylclarizg@gmail.com>
2022-10-02 18:53:30 +0800

    Department Repository

---
+++ app/Repositories/Department/DepartmentRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Department;
+
+use App\Repositories\Base\BaseRepositoryInterface;
+
+interface DepartmentRepositoryInterface extends BaseRepositoryInterface
+{
+    //
+}
