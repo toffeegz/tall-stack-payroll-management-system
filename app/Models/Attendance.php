61397b8c14f4440cde39c3ec9fc59e354baa4791
gezrylclariz <77222425+toffeegz@users.noreply.github.com>
2022-10-04 07:42:53 +0800

    Update Attendance Model -> fillable and use traits CreatedBy

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -5,16 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\CreatedBy;
 
 use App\Models\User;
 use App\Models\Project;
 
 class Attendance extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, CreatedBy;
 
-    protected $fillable = ['date', 'user_id', 'project_id'];
+    protected $fillable = [
+        'date', 
+        'user_id', 
+        'project_id',
+        'time_in',
+        'time_out',
+        'regular',
+        'late',
+        'undertime',
+        'overtime',
+        'night_differential',
+        'status',
+        'created_by',
+    ];
 
     public function user()
     {
