2b1ebfe05e297a98762b571daebc8b3e84b2cb56
gezrylclariz <gezrylclarizg@gmail.com>
2022-11-16 21:27:56 +0800

    Added functions in PayrollPeriodService

--- app/Services/PayrollPeriod/PayrollPeriodServiceInterface.php
+++ app/Services/PayrollPeriod/PayrollPeriodServiceInterface.php
@@ -5,4 +5,11 @@ namespace App\Services\PayrollPeriod;
 interface PayrollPeriodServiceInterface
 {
     public function mailToAdmin($frequency, $payout_dates);
+    public function payroll($data);
+    public function getSSSContributionAmount($salary);
+    public function getHDMFContributionAmount($monthly_basic_salary);
+    public function getPHICContributionAmount($monthly_basic_salary);
+    public function getTotalDaysPresentOrLate($user_id, $between_dates);
+    public function getTotalPaidLeaveHours($user_id, $between_dates);
+    public function getTotalAbsencesTardinesss($between_dates, $data);
 }
