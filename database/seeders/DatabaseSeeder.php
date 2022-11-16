0070ba46da7a2df7f2cad8602f2691cfab450707
gezrylclariz <gezrylclarizg@gmail.com>
2022-11-16 21:30:38 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,22 +14,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(Seeder::class);
-        $this->call(CompanyInformationSeeder::class);
-        $this->call(DepartmentSeeder::class);
-        $this->call(DesignationSeeder::class);
-        $this->call(DraftLogsSeeder::class);
-        $this->call(EarningSeeder::class);
-        $this->call(GrossTypeSeeder::class);
-        $this->call(HDMFContributionRateSeeder::class);
-        $this->call(LeaveSeeder::class);
-        $this->call(LeaveTypeSeeder::class);
-        $this->call(PayrollPeriodSeeder::class);
-        $this->call(PHICContributionRateSeeder::class);
-        $this->call(ProjectSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(SSSContributionModelSeeder::class);
-        $this->call(SSSContributionRateSeeder::class);
-        $this->call(UserSeeder::class);
+        $this->call([
+            CompanyInformationSeeder::class,
+            DepartmentSeeder::class,
+            DesignationSeeder::class,
+            DraftLogsSeeder::class,
+            EarningSeeder::class,
+            GrossTypeSeeder::class,
+            HDMFContributionRateSeeder::class,
+            // LeaveSeeder::class,
+            LeaveTypeSeeder::class,
+            PayrollPeriodSeeder::class,
+            PHICContributionRateSeeder::class,
+            ProjectSeeder::class,
+            RoleSeeder::class,
+            ScheduleSeeder::class,
+            SSSContributionModelSeeder::class,
+            SSSContributionRateSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
