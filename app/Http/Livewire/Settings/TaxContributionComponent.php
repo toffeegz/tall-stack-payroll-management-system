3d8a12cde194efab89d9f8062b16e52b23c132cc
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-30 00:29:43 +0800

    settings: tax contribution component added phic and hdmf table

--- app/Http/Livewire/Settings/TaxContributionComponent.php
+++ app/Http/Livewire/Settings/TaxContributionComponent.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire\Settings;
 use Livewire\Component;
 use App\Models\SssContributionModel;
 use App\Models\SssContributionRate;
+use App\Models\HdmfContributionRate;
+use App\Models\PhicContributionRate;
+use Carbon\Carbon;
 
 class TaxContributionComponent extends Component
 {
@@ -12,11 +15,23 @@ class TaxContributionComponent extends Component
     {
         return view('livewire.settings.tax-contribution-component',[
             'sss_contributions' => $this->sss_contribution_table,
+            'hdmf_contributions' => $this->hdmf_contribution_table,
+            'phic_contributions' => $this->phic_contribution_table,
         ]);
     }
 
     public function getSssContributionTableProperty()
     {
         return SssContributionRate::latest()->first();
+    }
+
+    public function getHdmfContributionTableProperty()
+    {
+        return HdmfContributionRate::where('year', Carbon::now()->format('Y'))->get();
+    }
+
+    public function getPhicContributionTableProperty()
+    {
+        return PhicContributionRate::where('year', Carbon::now()->format('Y'))->get();
     }
 }
--- resources/views/livewire/settings/tax-contribution-component.blade.php
+++ resources/views/livewire/settings/tax-contribution-component.blade.php
@@ -49,4 +49,78 @@
             </div>
         </div>
     </div>
+
+    <div>
+        <h2 class="my-4 font-bold text-stone-900">
+            HDMF Contribution Rates
+        </h2>
+        <!-- New Table -->
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-x-auto">
+                <table class="w-full whitespace-no-wrap">
+                    <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-stone-500 uppercase border-b  bg-stone-50 ">
+                        <th class="px-4 py-3">EE Share</th>
+                        <th class="px-4 py-3">ER Share</th>
+                        <th class="px-4 py-3">MSC Min</th>
+                        <th class="px-4 py-3">MSC Max</th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y "  >
+                        @foreach($hdmf_contributions as $hdmf_contribution)
+                            <tr class="text-stone-700 ">
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $hdmf_contribution->ee_share }}
+                                </td>
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $hdmf_contribution->er_share }}
+                                </td>
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $hdmf_contribution->msc_min }}
+                                </td>
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $hdmf_contribution->msc_max }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h2 class="my-4 font-bold text-stone-900">
+            PHIC Contribution Rates
+        </h2>
+        <!-- New Table -->
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-x-auto">
+                <table class="w-full whitespace-no-wrap">
+                    <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-stone-500 uppercase border-b  bg-stone-50 ">
+                        <th class="px-4 py-3">Premium Rate</th>
+                        <th class="px-4 py-3">MBS Min</th>
+                        <th class="px-4 py-3">MBS Max</th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y "  >
+                        @foreach($phic_contributions as $phic_contribution)
+                            <tr class="text-stone-700 ">
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $phic_contribution->premium_rate }}
+                                </td>
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $phic_contribution->mbs_min }}
+                                </td>
+                                <td class="px-4 py-2 text-xs">
+                                    {{ $phic_contribution->mbs_max }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
