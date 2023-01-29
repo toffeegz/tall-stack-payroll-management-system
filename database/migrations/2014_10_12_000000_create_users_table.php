34964f00392153e58eabd9f0d7b2a37f1a85875c
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-29 09:55:43 +0800

    issue sql constraint: fix by nullable in email_verified_at

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
