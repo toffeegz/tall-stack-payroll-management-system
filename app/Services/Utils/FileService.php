c7d8d1429d6cb8c526713ff32e2e7bcc5d336054
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-29 09:46:20 +0800

    Fix from local to public in FileService: issue - cant download template not found error

--- app/Services/Utils/FileService.php
+++ app/Services/Utils/FileService.php
@@ -17,14 +17,14 @@ class FileService implements FileServiceInterface
     public function download($folderName, $fileName)
     {
         $path = $this->basePath . $folderName ."/" . $fileName;
-        return Storage::disk('local')->download($path);
+        return Storage::disk('public')->download($path);
     }
 
     public function upload($folderName, $fileName, $file)
     {
         $fileName = $fileName . "." . $file->getClientOriginalExtension();
         $folderName = $this->basePath . $folderName;
-        $result = Storage::disk('local')->putFileAs($folderName, $file, $fileName);
+        $result = Storage::disk('public')->putFileAs($folderName, $file, $fileName);
         return $result;
     }
 }
