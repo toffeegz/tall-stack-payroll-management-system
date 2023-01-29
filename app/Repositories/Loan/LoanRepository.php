b7fe76d849e7033b31ae3700ad30fe3727347a69
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-29 22:25:42 +0800

    loan repository: get loans with balance; get balance; get amount to pay; get paid; get paid percentage;

--- app/Repositories/Loan/LoanRepository.php
+++ app/Repositories/Loan/LoanRepository.php
@@ -19,4 +19,36 @@ class LoanRepository extends BaseRepository implements LoanRepositoryInterface
     {
         parent::__construct($model);
     }
+
+    public function getTotalAmountToPay()
+    {
+
+    }
+
+    public function getLoansWithBalanceByUser(string $id)
+    {
+        return $this->model->where('user_id', $id)
+        ->where('balance', '!=', 0)
+        ->get();
+    }
+
+    public function getBalanceByUser(string $id)
+    {
+        return $this->getLoansWithBalanceByUser($id)->sum('balance');
+    }
+
+    public function getAmountToPayByUser(string $id)
+    {
+        return $this->getLoansWithBalanceByUser($id)->sum('total_amount_to_pay');
+    }
+
+    public function getPaidByUser(string $id)
+    {
+        return $this->getAmountToPayByUser($id) - $this->getBalanceByUser($id);
+    }
+
+    public function getPaidPercentageByUser(string $id)
+    {
+        return round($this->getPaidByUser($id)  / $this->getAmountToPayByUser($id) * 100);
+    }
 }
--- app/Repositories/Loan/LoanRepositoryInterface.php
+++ app/Repositories/Loan/LoanRepositoryInterface.php
@@ -6,5 +6,9 @@ use App\Repositories\Base\BaseRepositoryInterface;
 
 interface LoanRepositoryInterface extends BaseRepositoryInterface
 {
-    //
+    public function getLoansWithBalanceByUser(string $id);
+    public function getBalanceByUser(string $id);
+    public function getAmountToPayByUser(string $id);
+    public function getPaidByUser(string $id);
+    public function getPaidPercentageByUser(string $id);
 }
