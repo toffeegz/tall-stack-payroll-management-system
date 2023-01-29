d25889716118d704ff46dae04d8259d2b19d629e
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-29 21:26:30 +0800

    Update AttendanceRepository.php

--- app/Repositories/Attendance/AttendanceRepository.php
+++ app/Repositories/Attendance/AttendanceRepository.php
@@ -20,12 +20,15 @@ class AttendanceRepository extends BaseRepository implements AttendanceRepositor
         parent::__construct($model);
     }
 
-    public function getPendingForModal(string $search, array $relations, $paginate = 10, $sortByColumn = 'created_at', $sortBy = 'DESC')
+    public function getPendingForModal(string $search = '', array $relations, $paginate = 10, $sortByColumn = 'created_at', $sortBy = 'DESC')
     {
         $search = ['search' => $search];
-        return $this->model->filterPending($search)
+        return $this->model
+        ->filterPending($search)
         ->select('attendances.id', 'users.last_name', 'users.first_name', 'users.code', 'attendances.date', 'users.profile_photo_path')
         ->where('attendances.status', 4)
-        ->orderBy('attendances.' . $sortByColumn, $sortBy)->paginate($paginate);
+        ->orderBy('attendances.' . $sortByColumn, $sortBy)
+        ->get()->take($paginate);
+        
     }
 }
