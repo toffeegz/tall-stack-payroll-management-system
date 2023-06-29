71d0316816100a227f9442ab10784c5ae156a30b
gezrylclariz <gezrylclarizg@gmail.com>
2023-06-29 20:30:17 +0800

    fix: include archived user in loan installment

--- app/Models/LoanInstallment.php
+++ app/Models/LoanInstallment.php
@@ -15,6 +15,6 @@ class LoanInstallment extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
